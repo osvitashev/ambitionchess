08ede34c3b3e9480de414af82a0e34fe7659a4bc
osvitashev <48232648+osvitashev@users.noreply.github.com>
2022-01-19 15:38:12 -0500

    working mate in 6

--- src/puzzler/PuzzleSolver.java
+++ src/puzzler/PuzzleSolver.java
@@ -2,80 +2,84 @@ package puzzler;
 
 import gamestate.Board;
 import gamestate.MoveGen;
+import gamestate.Move;
 
 public class PuzzleSolver {
 	
-	private int[] movelist = new int[200];
+	private int[] movelist = new int[500];
 	private int movelist_size=0;
 	private int searchDepth=1;// value in halfmoves. Can only be an odd number.
 	
 	
 	// returns true is black is checkmated
-	boolean blackStep(Board brd, int depth) {
-		boolean blackLost=false;
+	private boolean toPlayAndWin_opponentStep(Board brd, int depth) {
+		boolean isWin=true;
 		int movelist_size_old = movelist_size;
 		movelist_size = MoveGen.generateLegalMoves(brd, movelist, movelist_size);
-		if(movelist_size == movelist_size_old && brd.isCheck()){
+		if(movelist_size == movelist_size_old && brd.isCheck()){//checkmate
 			
-			blackLost = true;
+			isWin = true;
 		}
 		else if(depth == searchDepth) {
-			blackLost = false;
+			isWin = false;
 		}
 		else if(movelist_size != movelist_size_old) {
 			for(int i=movelist_size_old; i<movelist_size; ++i) {
+				//here return isWin iff every opponent's move leads to isWin
 				int move = movelist[i];
 				
 				brd.makeMove(move);
-				blackLost = whiteStep(brd, depth+1);
+				isWin = toPlayAndWin_step(brd, depth+1);
 				brd.unmakeMove(move);
 				
-				if(blackLost)
+				if(!isWin)
 					break;
 			}
 		}
 		
 		movelist_size = movelist_size_old;
-		return blackLost;
+		return isWin;
 	}
 	
-	boolean whiteStep(Board brd, int depth) {
-		boolean blackLost=true;
+	private boolean toPlayAndWin_step(Board brd, int depth) {
+		boolean isWin=false;
 		int movelist_size_old = movelist_size;
 		movelist_size = MoveGen.generateLegalMoves(brd, movelist, movelist_size);
-		if(movelist_size != movelist_size_old || !brd.isCheck()){
+		if(movelist_size == movelist_size_old){
 			
-			blackLost = false;
+			isWin = false;
 		}
 		else if(depth == searchDepth) {
-			blackLost = false;
+			isWin = false;
 		}
 		else if(movelist_size != movelist_size_old) {
 			for(int i=movelist_size_old; i<movelist_size; ++i) {
+				//here return isWin if ANY of our moves evaluates to isWin
 				int move = movelist[i];
 				
 				brd.makeMove(move);
-				blackLost = blackStep(brd, depth+1);
+				isWin = toPlayAndWin_opponentStep(brd, depth+1);
 				brd.unmakeMove(move);
 				
-				if(!blackLost)
+				if(isWin)
 					break;
 			}
 		}
 		
 		movelist_size = movelist_size_old;
-		return blackLost;
+		return isWin;
 	}
 	
-	public int whiteToPlayAndWin(Board brd, int depth) {
+	public int toPlayAndWin(Board brd, int targetDepth) {
 		movelist_size=0;//reset
+		searchDepth = targetDepth;
 		int movelist_size_old = movelist_size;
 		movelist_size = MoveGen.generateLegalMoves(brd, movelist, movelist_size);
 		for(int i=movelist_size_old; i<movelist_size; ++i) {
 			int move = movelist[i];
 			
 			brd.makeMove(move);
-			boolean temp = blackStep(brd, depth+1);
+			boolean temp = toPlayAndWin_opponentStep(brd, 1);
 			brd.unmakeMove(move);
 			
 			if(temp)
