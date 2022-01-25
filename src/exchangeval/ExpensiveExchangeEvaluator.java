32704bb7b05f7eafba5bf5dd6aa7f95b0e065cd1
osvitashev <48232648+osvitashev@users.noreply.github.com>
2022-01-25 14:36:35 -0500

    toOccupy() - tests without king involvement

--- src/exchangeval/ExpensiveExchangeEvaluator.java
+++ src/exchangeval/ExpensiveExchangeEvaluator.java
@@ -19,14 +19,15 @@ public class ExpensiveExchangeEvaluator {
 	private int square;// square the evaluation is for.
 
 	/**
-	 * Generates all legal captures in a position. It is caller's responsibility to
-	 * ensure that the ones played through fall on the correct square.
+	 * Generates all legal captures, capture promotions and pawn pushes(which should
+	 * be filtered out) in a position. It is caller's responsibility to ensure that
+	 * the ones played through fall on the correct square.
 	 * 
 	 * @param brd
 	 * @param movepool
 	 * @return
 	 */
-	private static int generateExchangeMoves(Board brd, MovePool movepool) {
+	private static int generateExchangeCaptureMoves(Board brd, MovePool movepool) {
 		MoveGen.generatePawnCaptures(brd, movepool);
 		MoveGen.generatePawnPromotionsAndCapturePromotions(brd, movepool);
 		MoveGen.generateRookCaptures(brd, movepool);
@@ -38,7 +39,28 @@ public class ExpensiveExchangeEvaluator {
 	}
 
 	/**
+	 * Generates legal non-captures in a position. Only includes NORMAL and PROMO
+	 * and DOUBLE_PUSH move type. It is caller's responsibility to ensure that the
+	 * ones played through fall on the correct square.
+	 * 
+	 * @param brd
+	 * @param movepool
+	 * @return
+	 */
+	private static int generateExchangeNonCaptureMoves(Board brd, MovePool movepool) {
+		MoveGen.generatePawnMoves(brd, movepool);
+		MoveGen.generatePawnPromotionsAndCapturePromotions(brd, movepool);
+		MoveGen.generateRookMoves(brd, movepool);
+		MoveGen.generateKnightMoves(brd, movepool);
+		MoveGen.generateBishopMoves(brd, movepool);
+		MoveGen.generateQueenMoves(brd, movepool);
+		MoveGen.generateKingMoves(brd, movepool);
+		return movepool.size();
+	}
+
+	/**
 	 * Negamax/alpha beta type recursive algorithm.
+	 * 
 	 * @param brd
 	 * @param isOpponent - determines whether we are minimizing or maximizing.
 	 * @return
@@ -46,7 +68,7 @@ public class ExpensiveExchangeEvaluator {
 	private boolean toCaptureAndOccupy_step(Board brd, boolean isOpponent) {
 		boolean isDone = isOpponent;
 		int movelist_size_old = movepool.size();
-		generateExchangeMoves(brd, movepool);
+		generateExchangeCaptureMoves(brd, movepool);
 		for (int i = movelist_size_old; i < movepool.size(); ++i) {
 			int move = movepool.get(i);
 			if ((Move.getMoveType(move) == MoveType.CAPTURE || Move.getMoveType(move) == MoveType.PROMO_CAPTURE) && square == Move.getSquareTo(move)) {
@@ -62,27 +84,58 @@ public class ExpensiveExchangeEvaluator {
 	}
 
 	/**
+	 * Negamax/alpha beta type recursive algorithm.
+	 * 
+	 * @param brd
+	 * @param isOpponent - determines whether we are minimizing or maximizing.
+	 * @return
+	 */
+	private boolean toMoveAndOccupy_step(Board brd, boolean isOpponent) {
+		boolean isDone = isOpponent;
+		int movelist_size_old = movepool.size();
+		generateExchangeNonCaptureMoves(brd, movepool);
+		for (int i = movelist_size_old; i < movepool.size(); ++i) {
+			int move = movepool.get(i);
+			if ((Move.getMoveType(move) == MoveType.PROMO || Move.getMoveType(move) == MoveType.NORMAL || Move.getMoveType(move) == MoveType.DOUBLE_PUSH)
+					&& square == Move.getSquareTo(move)) {
+				brd.makeMove(move);
+				isDone = toCaptureAndOccupy_step(brd, !isOpponent);
+				brd.unmakeMove(move);
+				if (isDone && !isOpponent || !isDone && isOpponent)
+					break;
+			}
+		}
+		movepool.resize(movelist_size_old);
+		return isDone;
+	}
+
+	/**
 	 * Determines if a given square can be occupied by a series of exchanges.
-	 * Disregards material cost - only is occupation can be forced. En Passant is
+	 * Disregards material cost - only if occupation can be forced. En Passant is
 	 * not considered because location of the captured pawn does not match the
-	 * location of the pawn making the capture.
-	 * IMPORTANT: player argument overrides FEN side-to-move.
+	 * location of the pawn making the capture. IMPORTANT: player argument overrides
+	 * FEN side-to-move.
 	 * 
 	 * @param brd
 	 * @param square
+	 * @param player
 	 * @return
 	 */
-	public boolean toCaptureAndOccupy(Board brd, int square, int player) {
+	public boolean toOccupy(Board brd, int square, int player) {
 		DebugLibrary.validatePlayer(player);
 		DebugLibrary.validateSquare(square);
+		if(brd.getPlayerAt(square) == player)
+			return false;//friendly capture
 		this.square = square;
 		movepool.clear();
-		//This is an insanely horrible substitute for a null move...
-		if(brd.getPlayerToMove() != player) {
+		// This is an insanely horrible substitute for a null move...
+		if (brd.getPlayerToMove() != player) {
 			String fen = brd.toFEN();
-			fen = fen.replace((player == Player.WHITE)? " b " : " w ", (player == Player.BLACK)? " b " : " w ");
+			fen = fen.replace((player == Player.WHITE) ? " b " : " w ", (player == Player.BLACK) ? " b " : " w ");
 			brd = new Board(fen);
 		}
+		if(brd.getPlayerAt(square) == Player.NO_PLAYER)
+			return toMoveAndOccupy_step(brd, false);
 		return toCaptureAndOccupy_step(brd, false);
 	}
 
