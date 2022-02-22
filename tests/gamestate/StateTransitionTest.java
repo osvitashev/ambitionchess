298924663a35410d3b9a1172eb69ac657f1a291f
osvitashev <48232648+osvitashev@users.noreply.github.com>
2022-02-21 19:43:56 -0500

    working move sequence testing

--- tests/gamestate/StateTransitionTest.java
+++ tests/gamestate/StateTransitionTest.java
@@ -11,15 +11,16 @@ class StateTransitionTest {
 	private static MovePool test_movepool = new MovePool();
 	private static Gamestate test_board = new Gamestate();
 
-	public static void runTest(String fen, String... uci_moves) {
-
+	public static void runTest(String fen, String correctFen, String... uci_moves) {
 		test_board.loadFromFEN(fen);
-		runTest(test_board, Arrays.asList(uci_moves).subList(0, uci_moves.length));
+		runTest(test_board,correctFen, Arrays.asList(uci_moves).subList(0, uci_moves.length));
+		assertEquals(fen, test_board.toFEN());
 	}
 
-	public static void runTest(Gamestate board, List<String> uci_moves) {
+	public static void runTest(Gamestate board, String correctFen, List<String> uci_moves) {
 		board.validateState();
 		if (uci_moves.size()==0) {
+			assertEquals(correctFen, test_board.toFEN());
 			return;
 		}
 		int test_movelist_size_old = test_movepool.size();
@@ -33,9 +34,10 @@ class StateTransitionTest {
 			if(Move.toUCINotation(test_movepool.get(i)).equals(uci_moves.get(0))) {
 				found = true;
 				int move = test_movepool.get(i);
+				//System.out.println("making: " + uci_moves.get(0));
 				board.makeMove(move);
 				
-				runTest(board, uci_moves.subList(1, uci_moves.size()));
+				runTest(board,correctFen, uci_moves.subList(1, uci_moves.size()));
 				
 				board.unmakeMove(move);
 				break;
@@ -47,7 +49,7 @@ class StateTransitionTest {
 
 	@Test
 	void test() {
-		runTest("8/8/8/3k1n2/8/PP6/2P5/R3K3 w Q - 0 1", "e1c1", "f5d4", "c2c4", "d5d6", "d1d4", "d6c6");
+		runTest("8/8/8/3k1n2/8/PP6/2P5/R3K3 w Q - 0 1", "8/8/2k5/8/2PR4/PP6/8/2K5 w - - 1 4", "e1c1", "f5d4", "c2c4", "d5d6", "d1d4", "d6c6");
 	}
 
 }
