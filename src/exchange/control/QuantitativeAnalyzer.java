1fc25be379d693ec98c128ef6d5ada5c029db7fe
osvitashev <48232648+osvitashev@users.noreply.github.com>
2023-07-12 19:57:30 -0400

    analyzer internal state

--- src/exchange/control/QuantitativeAnalyzer.java
+++ src/exchange/control/QuantitativeAnalyzer.java
@@ -3,19 +3,61 @@ package exchange.control;
 import static util.BitField32.getBits;
 import static util.BitField32.setBits;
 
+import java.util.Arrays;
+
+import gamestate.Bitboard;
+import gamestate.DebugLibrary;
 import gamestate.Gamestate;
 import gamestate.GlobalConstants.PieceType;
+import gamestate.GlobalConstants.Player;
 import gamestate.GlobalConstants.Square;
+import util.BitvectorTranspose;
+
+
+/**
+ * 
+ * AttackSets are not going to be modified during the evaluation,  but this class deos contain other data fields which are used as temporary variables and which are going to be overwritten.
+ *
+ */
+
+//This should have a reference to const board state. and separate initializer for permanent and non-permannet member data
 
 public class QuantitativeAnalyzer {
+	private static final int[] COSTS = { 1, 3, 3, 5, 9};
+	
+	/**
+	 * Only used for ordering. Does not represent exchange evaluation cost!
+	 * Notice that King does not have an assigned value!!!!
+	 * @param pieceType
+	 * @return
+	 */
+	public static int getPieceValue(int pieceType) {
+		DebugLibrary.validatePieceType(pieceType);
+		return COSTS[pieceType];
+	}
+	
+	
 	//TODO: remove the class and replace it with two parallel arrays
-	class AttackSet{
+	class AttackSet implements Comparable<AttackSet>{
+		@Override
+	    public int compareTo(AttackSet as) {
+	        return this.getPieceType()-as.getPieceType();
+	    }
+		
 		private int data;
 		private long attacks;
 		
 		private AttackSet(long attacks) {
 			this.attacks = attacks;
 		}
+		
+		/**
+		  	memory packing: 
+			start	length	name
+			0		3		PieceType - is set to PAWN for pawn attacks and pushes
+			3		6		Square origin - is not meaningful for pawns
+			9
+		 */
 
 		private int getPieceType() {
 			return getBits(data, 0, 3);
@@ -44,61 +86,98 @@ public class QuantitativeAnalyzer {
 		}
 	}
 	
-	/**
-	 * 
-	 
-	  * 
-	  	memory packing: 
-		start	length	name
-		0		3		PieceType - is set to PAWN for pawn attacks and pushes
-		3		6		Square origin - is not meaningful for pawns
-		9
-
-	 @formatter:on
-	 */
-	final static class AttackSetUtil{
-
+	void sortSets() {
+		Arrays.sort(attackSets[0], 0, length_attackSets[0]);
+		Arrays.sort(attackSets[1], 0, length_attackSets[1]);
 	}
 	
+	private Gamestate brd;
 	private AttackSet attackSets[][] = new AttackSet[2][30];
-	private int length[] = new int[2];
+	private int length_attackSets[] = new int[2];
+	private BitvectorTranspose temp_exchangeGainLoss = new BitvectorTranspose();
 	
-	int size(int player) {
-		return length[player];
+	int size_attackSet(int player) {
+		return length_attackSets[player];
+	}
+	
+	/**
+	 * A getter. Is only meant to be used for testing/debigging.
+	 * @param sq
+	 * @return
+	 */
+	int getTemp_exchangeGainLoss(int sq) {
+		return temp_exchangeGainLoss.indexToScalar(sq);
+	}
+	
+	
+	/**
+	 * Populates Exchange gain/loss parallel scalar with the weights of pieces currently on the board.
+	 * Positive values for both players.
+	 */
+	void reinitializeGainLossToPieceValues() {
+		temp_exchangeGainLoss.clear();
+		
+		long mask = brd.getPieces(PieceType.ROOK);
+		temp_exchangeGainLoss.addScalar(mask, getPieceValue(PieceType.ROOK) );
+		
+		mask = brd.getPieces(PieceType.KNIGHT);
+		temp_exchangeGainLoss.addScalar(mask, getPieceValue(PieceType.KNIGHT));
+		
+		mask = brd.getPieces(PieceType.BISHOP);
+		temp_exchangeGainLoss.addScalar(mask, getPieceValue(PieceType.BISHOP));
+		
+		mask = brd.getPieces(PieceType.QUEEN);
+		temp_exchangeGainLoss.addScalar(mask, getPieceValue(PieceType.QUEEN));
+		
+		mask = brd.getPieces(PieceType.PAWN);
+		temp_exchangeGainLoss.addScalar(mask, getPieceValue(PieceType.PAWN));
 	}
 	
 
-	void reset() {
-		length[0] =0;
-		length[1]=0;
+	
+	private long output_winningCaptures;//target squares where a safe/winning capture can be made. Shared by both players
+	private long output_neutralCaptures;//target squares where a break-even capture can be made. Shared by both players
+	private long output_badCaptures;
+	public void populateCapturesBothPlayers() {
+		long temp_captureHappened=0;//used to distinguish between 'White Knight at f4' and 'White Knight at f4 is taken'. Needed for after the exchange calculation.
+		
+		
+		
 	}
+	
 	
 	void addAttackSetPieceTypeSquare(int pt, int sq, int player, long attacks) {
+		////TODO =>>>>>>>> This is VERY wrong. We do not want to be creating objects on-demand. EVER!!!!!
+		// THOUUGH, this will be refactored to not have this class anyways...
 		AttackSet as = new AttackSet(attacks);
 		as.setPieceType(pt);
 		as.setSquare(sq);
-		attackSets[player][length[player]++]=as;
+		attackSets[player][length_attackSets[player]++]=as;
 	}
 	
 	void addAttackSetPawn( int player, long attacks) {
-		AttackSet as = new AttackSet(attacks);
-		attackSets[player][length[player]++]=as;
+		addAttackSetPieceTypeSquare(PieceType.PAWN, Square.A1, player, attacks);
 	}
 	
 	public String toString() {
 		String ret="";
 		ret += "White:\n";
-		for(int i=0; i<length[0];++i)
+		for(int i=0; i<length_attackSets[0];++i)
 			ret+=attackSets[0][i].toString()+"\n";
 		ret += "Black:\n";
-		for(int i=0; i<length[1];++i)
+		for(int i=0; i<length_attackSets[1];++i)
 			ret+=attackSets[1][i].toString()+"\n";
 		return ret;
 	}
 	
-	//try using mockito for this
-	public void generateFromPosition(final Gamestate brd) {
-		
+	/*
+	 * Should be the first thing called when analyzing a new position.
+	 * 
+	 */
+	public void initializeBoardState(final Gamestate brd) {
+		this.brd = brd;
+		length_attackSets[Player.WHITE]=0;
+		length_attackSets[Player.WHITE]=0;
 	}
 	
 	
--- tests/exchange/control/QuantitativeAnalyzerMockDataTest.java
+++ tests/exchange/control/QuantitativeAnalyzerMockDataTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import gamestate.Gamestate;
 import gamestate.GlobalConstants.PieceType;
 import gamestate.GlobalConstants.Player;
 import gamestate.GlobalConstants.Square;
@@ -15,7 +16,6 @@ class QuantitativeAnalyzerMockDataTest {
 		QuantitativeAnalyzer qa = new QuantitativeAnalyzer();
 		qa.addAttackSetPieceTypeSquare(PieceType.KING, Square.A2, Player.WHITE, 0x33);
 		qa.addAttackSetPieceTypeSquare(PieceType.KING, Square.G7, Player.BLACK, 0x787878);
-		
 		qa.addAttackSetPieceTypeSquare(PieceType.KNIGHT, Square.A3, Player.WHITE, 0x65);
 		qa.addAttackSetPieceTypeSquare(PieceType.KNIGHT, Square.F5, Player.BLACK, 0xc8);
 		qa.addAttackSetPieceTypeSquare(PieceType.PAWN, Square.E5, Player.BLACK, 0x854);
@@ -29,8 +29,163 @@ class QuantitativeAnalyzerMockDataTest {
 				+ "{P -> 0x854}\n"
 				+ "", qa.toString());
 		
-		System.out.println(qa.toString());
+		qa.sortSets();
+		assertEquals("White:\n"
+				+ "{N a3 -> 0x65}\n"
+				+ "{K a2 -> 0x33}\n"
+				+ "Black:\n"
+				+ "{P -> 0x854}\n"
+				+ "{N f5 -> 0xc8}\n"
+				+ "{K g7 -> 0x787878}\n"
+				+ "", qa.toString());
 		
+		qa.addAttackSetPieceTypeSquare(PieceType.QUEEN, Square.A4, Player.WHITE, 0x1);
+		qa.addAttackSetPieceTypeSquare(PieceType.BISHOP, Square.A5, Player.WHITE, 0x2);
+		qa.addAttackSetPieceTypeSquare(PieceType.PAWN, Square.A1, Player.WHITE, 0xff1);
+		qa.addAttackSetPieceTypeSquare(PieceType.PAWN, Square.A1, Player.WHITE, 0xff2);
+		qa.addAttackSetPieceTypeSquare(PieceType.ROOK, Square.A6, Player.WHITE, 0x3);
+		
+		qa.sortSets();
+		assertEquals("White:\n"
+				+ "{P -> 0xff1}\n"
+				+ "{P -> 0xff2}\n"
+				+ "{N a3 -> 0x65}\n"
+				+ "{B a5 -> 0x2}\n"
+				+ "{R a6 -> 0x3}\n"
+				+ "{Q a4 -> 0x1}\n"
+				+ "{K a2 -> 0x33}\n"
+				+ "Black:\n"
+				+ "{P -> 0x854}\n"
+				+ "{N f5 -> 0xc8}\n"
+				+ "{K g7 -> 0x787878}\n"
+				+ "", qa.toString());
+		//System.out.println(qa.toString());
+	}
+	
+
+	/**
+	 * normal flow:
+	 * QuantitativeAnalyzer qa = new QuantitativeAnalyzer();
+	 * qa.initializeFromGamestate(... gamestate);
+	 * ... at this point all of the *output* variables are populated
+	 * 
+	 * public void initializeFromGamestate(Gamestate brd){
+	 * 	populateAttackSets();
+	 * 	resetTemp();
+	 * 	calculateCapturesBothPlayers();
+	 * 	resetTemp;
+	 * 	calculateQuietMoves(white);
+	 * 	resetTemp;
+	 * 	calculateQuietMoves(black);
+	 * 
+	 * 
+	 * }
+	 * 
+	 * 
+	 * 
+	 */
+	
+	@Test
+	void reinitializeGainLossToPieceValuesTest() {
+		Gamestate brd;
+		QuantitativeAnalyzer qa = new QuantitativeAnalyzer();
+		brd = new Gamestate("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1");
+		qa.initializeBoardState(brd);
+		qa.reinitializeGainLossToPieceValues();
+		
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.ROOK), qa.getTemp_exchangeGainLoss(Square.A1));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.KNIGHT), qa.getTemp_exchangeGainLoss(Square.B1));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.BISHOP), qa.getTemp_exchangeGainLoss(Square.C1));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.QUEEN), qa.getTemp_exchangeGainLoss(Square.D1));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.E1));//king
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.BISHOP), qa.getTemp_exchangeGainLoss(Square.F1));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.KNIGHT), qa.getTemp_exchangeGainLoss(Square.G1));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.ROOK), qa.getTemp_exchangeGainLoss(Square.H1));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.A2));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.B2));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.C2));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.D2));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.E4));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.F2));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.G2));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.H2));
+		
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.ROOK), qa.getTemp_exchangeGainLoss(Square.A8));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.KNIGHT), qa.getTemp_exchangeGainLoss(Square.B8));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.BISHOP), qa.getTemp_exchangeGainLoss(Square.C8));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.QUEEN), qa.getTemp_exchangeGainLoss(Square.D8));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.E8));//king
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.BISHOP), qa.getTemp_exchangeGainLoss(Square.F8));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.KNIGHT), qa.getTemp_exchangeGainLoss(Square.G8));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.ROOK), qa.getTemp_exchangeGainLoss(Square.H8));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.A7));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.B7));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.C7));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.D7));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.E5));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.F7));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.G7));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.H7));
+		//empty
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.E2));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.E7));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.B3));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.C4));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.D6));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.E6));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.E3));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.H6));
+		
+		brd = new Gamestate("3r1rk1/pb1q1pbp/1pnp1np1/2p1p1B1/4P3/2NP1N2/PPPQBPPP/2KR3R w - - 0 1");
+		qa.initializeBoardState(brd);
+		qa.reinitializeGainLossToPieceValues();
+		
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.ROOK), qa.getTemp_exchangeGainLoss(Square.D1));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.KNIGHT), qa.getTemp_exchangeGainLoss(Square.C3));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.BISHOP), qa.getTemp_exchangeGainLoss(Square.E2));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.QUEEN), qa.getTemp_exchangeGainLoss(Square.D2));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.C1));//king
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.BISHOP), qa.getTemp_exchangeGainLoss(Square.G5));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.KNIGHT), qa.getTemp_exchangeGainLoss(Square.F3));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.ROOK), qa.getTemp_exchangeGainLoss(Square.H1));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.A2));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.B2));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.C2));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.D3));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.E4));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.F2));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.G2));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.H2));
+		
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.ROOK), qa.getTemp_exchangeGainLoss(Square.D8));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.KNIGHT), qa.getTemp_exchangeGainLoss(Square.C6));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.BISHOP), qa.getTemp_exchangeGainLoss(Square.B7));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.QUEEN), qa.getTemp_exchangeGainLoss(Square.D7));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.E8));//king
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.BISHOP), qa.getTemp_exchangeGainLoss(Square.G7));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.KNIGHT), qa.getTemp_exchangeGainLoss(Square.F6));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.ROOK), qa.getTemp_exchangeGainLoss(Square.F8));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.A7));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.B6));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.C5));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.D6));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.E5));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.F7));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.G6));
+		assertEquals(QuantitativeAnalyzer.getPieceValue(PieceType.PAWN), qa.getTemp_exchangeGainLoss(Square.H7));
+		
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.A1));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.B1));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.E1));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.F1));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.G1));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.A8));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.B8));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.C8));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.E8));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.H8));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.C7));
+		assertEquals(0, qa.getTemp_exchangeGainLoss(Square.E7));
 	}
 
 }
