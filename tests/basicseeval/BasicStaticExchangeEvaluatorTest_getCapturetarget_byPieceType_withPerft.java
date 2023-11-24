6f241eacdcc333ba14a1c93a291c56971cfdb63d
osvitashev <48232648+osvitashev@users.noreply.github.com>
2023-11-23 19:47:33 -0500

    rolling checksum test with CRC32

--- tests/basicseeval/BasicStaticExchangeEvaluatorTest_getCapturetarget_byPieceType_withPerft.java
+++ tests/basicseeval/BasicStaticExchangeEvaluatorTest_getCapturetarget_byPieceType_withPerft.java
@@ -2,6 +2,9 @@ package basicseeval;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.nio.ByteBuffer;
+import java.util.zip.CRC32;
+
 import org.junit.jupiter.api.Test;
 
 import gamestate.Gamestate;
@@ -17,26 +20,68 @@ class BasicStaticExchangeEvaluatorTest_getCapturetarget_byPieceType_withPerft {
 	private MovePool movepool = new MovePool();
 	private MoveGen test_move_generator = new MoveGen();
 	
+	ByteBuffer buffer1, buffer2, buffer3;
+	CRC32 crc1, crc2, crc3;
+	
+	String checkSum;
+	
 	public long testPerft(String fen, int depth) {
 		test_game.loadFromFEN(fen);
-		hashValue=0;
-		return perft(test_game, depth, 1);
+		crc1 = new CRC32();
+		crc2 = new CRC32();
+		crc3 = new CRC32();
+		
+		crc1.reset();
+		crc2.reset();
+		crc3.reset();
+		
+		buffer1 = ByteBuffer.allocate(2 * 6 * 8);// 2 players * 6 piece types * 3 output types * 8 bytes per long.
+		buffer2 = ByteBuffer.allocate(2 * 6 * 8);// 2 players * 6 piece types * 3 output types * 8 bytes per long.
+		buffer3 = ByteBuffer.allocate(2 * 6 * 8);// 2 players * 6 piece types * 3 output types * 8 bytes per long.
+
+		long t = perft(test_game, depth, 1);
+		checkSum = getChecksum();
+		return t;
 	}
 	
-	long hashValue=0;
 	
 	private void updateHashValue() {
 		test_eval.initialize();
 		test_eval.evaluateCaptures();
 		
+		buffer1.clear();
+		buffer2.clear();
+		buffer3.clear();
+		buffer1.mark();
+		buffer2.mark();
+		buffer3.mark();
 		for (int player : Player.PLAYERS) {
 			for (int pieceType : PieceType.PIECE_TYPES) {
-				hashValue ^= test_eval.getOutput_capture_winning(player, pieceType);
-				hashValue ^= test_eval.getOutput_capture_neutral(player, pieceType);
-				hashValue ^= test_eval.getOutput_capture_losing(player, pieceType);
+				buffer1.putLong(test_eval.getOutput_capture_winning(player, pieceType));
+				buffer2.putLong(test_eval.getOutput_capture_neutral(player, pieceType));
+				buffer3.putLong(test_eval.getOutput_capture_losing(player, pieceType));
+				
+//				crc.update((int) (test_eval.getOutput_capture_winning(player, pieceType) >>> 32));
+//				crc.update((int) test_eval.getOutput_capture_winning(player, pieceType));
+//				
+//				crc.update((int) (test_eval.getOutput_capture_neutral(player, pieceType) >>> 32));
+//				crc.update((int) test_eval.getOutput_capture_neutral(player, pieceType));
+//				
+//				crc.update((int) (test_eval.getOutput_capture_losing(player, pieceType) >>> 32));
+//				crc.update((int) test_eval.getOutput_capture_losing(player, pieceType));
 			}
 		}
 		
+		buffer1.reset();
+		buffer2.reset();
+		buffer3.reset();
+		crc1.update(buffer1);
+		crc2.update(buffer2);
+		crc3.update(buffer3);
+	}
+	
+	String getChecksum() {
+		return String.format("%08X", crc1.getValue()) + String.format("%08X", crc2.getValue()) + String.format("%08X", crc3.getValue());
 	}
 
 	private long perft(Gamestate board, int depth, int ply) {
@@ -74,43 +119,48 @@ class BasicStaticExchangeEvaluatorTest_getCapturetarget_byPieceType_withPerft {
 		return nodes;
 	}
 	
+	static boolean skipAssertions = false;
+	
+	void test(String fen, int depth, String expectedChecksum) {
+		testPerft(fen, depth);
+		if(!skipAssertions)
+			assertEquals(expectedChecksum, checkSum);
+		System.out.println("collected checkSum: " + checkSum);
+	}
+	
 	@Test
 	void testWithPerft() {
-		testPerft("8/q2rN2p/6p1/4pR1b/R2RkN2/P1Q5/1P2n1K1/6b1 b - - 0 1", 1);
-		assertEquals(0x19401031041040l, hashValue);
-		System.out.println("collected hashValue: " + String.format("0x%08X", hashValue));
+		test("qk1n1r2/pb2P3/3n4/8/p1P1Q1P1/7P/1R3KQ1/1R5B w - - 0 1", 1, "A5F49E5DBAF465AE9DCE3ACB");
+		test("qk1n1r2/pb2P3/3n4/8/p1P1Q1P1/7P/1R3KQ1/1R5B w - - 0 1", 2, "100C6C5A228FEA769A4FE718");
+		test("qk1n1r2/pb2P3/3n4/8/p1P1Q1P1/7P/1R3KQ1/1R5B w - - 0 1", 3, "9A944BD17F5C8A8EDE1823A2");
+		test("qk1n1r2/pb2P3/3n4/8/p1P1Q1P1/7P/1R3KQ1/1R5B w - - 0 1", 4, "667716A46F1A5B702AC4413F");
+		test("qk1n1r2/pb2P3/3n4/8/p1P1Q1P1/7P/1R3KQ1/1R5B w - - 0 1", 5, "E48A0D1EDEB8350598A60D8B");
+		test("3r2b1/ppnr4/1Np1knpp/3R1p2/1N6/1BKQ1qp1/PP4bP/3R4 w - - 0 1", 1, "4C0B6FC01EDED72C6328DDA9");
+		test("3r2b1/ppnr4/1Np1knpp/3R1p2/1N6/1BKQ1qp1/PP4bP/3R4 w - - 0 1", 2, "4274FA368C6DFEE4EBD30D12");
+		test("3r2b1/ppnr4/1Np1knpp/3R1p2/1N6/1BKQ1qp1/PP4bP/3R4 w - - 0 1", 3, "1E74D0EADE998CF9435A8BF0");
+		test("3r2b1/ppnr4/1Np1knpp/3R1p2/1N6/1BKQ1qp1/PP4bP/3R4 w - - 0 1", 4, "25834752B48ADAAA5C059BE8");
+		test("3r2b1/ppnr4/1Np1knpp/3R1p2/1N6/1BKQ1qp1/PP4bP/3R4 w - - 0 1", 5, "6D337D5C0228D775BAA48171");
+		test("1k6/1pp2npb/r2N1rq1/8/2p1N3/3R2P1/QP3P2/3R2K1 w - - 0 1", 1, "AB17EFF87396A4820A21AC29");
+		test("1k6/1pp2npb/r2N1rq1/8/2p1N3/3R2P1/QP3P2/3R2K1 w - - 0 1", 2, "16D2C64A85AFF23B0347FA5F");
+		test("1k6/1pp2npb/r2N1rq1/8/2p1N3/3R2P1/QP3P2/3R2K1 w - - 0 1", 3, "4F3422E2D89A020022470491");
+		test("1k6/1pp2npb/r2N1rq1/8/2p1N3/3R2P1/QP3P2/3R2K1 w - - 0 1", 4, "551A2B6E6D7601D5A2911962");
+		test("1k6/1pp2npb/r2N1rq1/8/2p1N3/3R2P1/QP3P2/3R2K1 w - - 0 1", 5, "C9CF27DB6FA8645FFE05BC32");
+		test("2r5/2r5/4kn2/5r1p/6P1/3K4/2Q5/8 w - - 0 1", 1, "F64F3D965F41F7162348C89F");
+		test("2r5/2r5/4kn2/5r1p/6P1/3K4/2Q5/8 w - - 0 1", 2, "FEBEBC537B1ED76C1DF71CDB");
+		test("2r5/2r5/4kn2/5r1p/6P1/3K4/2Q5/8 w - - 0 1", 3, "A4028713B04A6479131F1648");
+		test("2r5/2r5/4kn2/5r1p/6P1/3K4/2Q5/8 w - - 0 1", 4, "F4006DF1551082EFF20ED06D");
+		test("8/q2rN2p/6p1/4pR1b/R2RkN2/P1Q5/1P2n1K1/6b1 b - - 0 1", 1, "CE2C1C417A3CB2712F1D3592");
+		test("8/q2rN2p/6p1/4pR1b/R2RkN2/P1Q5/1P2n1K1/6b1 b - - 0 1", 2, "6B75A467269B5B024B88CE3B");
+		test("8/q2rN2p/6p1/4pR1b/R2RkN2/P1Q5/1P2n1K1/6b1 b - - 0 1", 3, "E76157EA400B1324E6202E87");
+		test("8/q2rN2p/6p1/4pR1b/R2RkN2/P1Q5/1P2n1K1/6b1 b - - 0 1", 4, "DB158CF41F6E31D0B583B3A9");
+		test("8/q2rN2p/6p1/4pR1b/R2RkN2/P1Q5/1P2n1K1/6b1 b - - 0 1", 5, "AE8D53D82465C9C7BA9ADD3D");
+		test("8/q2rN2p/6p1/4pR1b/R2RkN2/P1Q5/1P2n1K1/6b1 b - - 0 1", 6, "A8CDA178250272C8F518DA8C");
+		test("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 3, "F9B094172EE79C127F62CF17");
+		test("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 4, "3F330006ADACB2F068A62CB1");
+		test("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 5, "5BF3935CCE30BB531B6A61F8");
 		
-		testPerft("8/q2rN2p/6p1/4pR1b/R2RkN2/P1Q5/1P2n1K1/6b1 b - - 0 1", 2);
-		assertEquals(0x9003011001040l, hashValue);
-		System.out.println("collected hashValue: " + String.format("0x%08X", hashValue));
-		
-		testPerft("8/q2rN2p/6p1/4pR1b/R2RkN2/P1Q5/1P2n1K1/6b1 b - - 0 1", 3);
-		assertEquals(0x108509201400l, hashValue);
-		System.out.println("collected hashValue: " + String.format("0x%08X", hashValue));
-		
-		testPerft("8/q2rN2p/6p1/4pR1b/R2RkN2/P1Q5/1P2n1K1/6b1 b - - 0 1", 4);
-		assertEquals(0x630C0BDF4149DAD8l, hashValue);
-		System.out.println("collected hashValue: " + String.format("0x%08X", hashValue));
-		
-		testPerft("8/q2rN2p/6p1/4pR1b/R2RkN2/P1Q5/1P2n1K1/6b1 b - - 0 1", 5);
-		assertEquals(0xE6177895C7CFE174l, hashValue);
-		System.out.println("collected hashValue: " + String.format("0x%08X", hashValue));
-		
-		testPerft("8/q2rN2p/6p1/4pR1b/R2RkN2/P1Q5/1P2n1K1/6b1 b - - 0 1", 6);
-		assertEquals(0x5EF8DA9BA0B8D11l, hashValue);
-		System.out.println("collected hashValue: " + String.format("0x%08X", hashValue));
-		
-		testPerft("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 3);
-		assertEquals(0xA854513252AF0A8Cl, hashValue);
-		System.out.println("collected hashValue: " + String.format("0x%08X", hashValue));
-		
-		testPerft("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 4);
-		assertEquals(0x2B8D35B7E4748920l, hashValue);
-		System.out.println("collected hashValue: " + String.format("0x%08X", hashValue));
-		
-		testPerft("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 5);
-		assertEquals(0x7BFC19AB2B9440C5l, hashValue);
-		System.out.println("collected hashValue: " + String.format("0x%08X", hashValue));
+		if(skipAssertions)
+			fail(">>>skipped all asserions!!!!!!");
 		
 	}
 
