87d0bdc636dc53f6be81f1d248a731617ed8fa91
osvitashev <48232648+osvitashev@users.noreply.github.com>
2023-12-07 19:40:46 -0500

    added to

--- src/analysis/Interaction.java
+++ src/analysis/Interaction.java
@@ -8,9 +8,11 @@ import gamestate.GlobalConstants.Square;
 public class Interaction {
 	
 	private static final int TYPE_OVERPROTECTION = 1;
+	private static final int TYPE_BARELY_ADEQUATE_GUARD = 2;
+	private static final int TYPE_ADEQUATE_GUARD = 3;
 	
 	//types width: 6 bits!
-	private static final int[] TYPES = {TYPE_OVERPROTECTION};
+	private static final int[] TYPES = {TYPE_OVERPROTECTION, TYPE_BARELY_ADEQUATE_GUARD, TYPE_ADEQUATE_GUARD};
 	
 	public static int getType(int interaction) {
 		int ret = getBits(interaction, 0, 6);
@@ -21,11 +23,22 @@ public class Interaction {
 	public static String toString(int interaction) {
 		String ret;
 		int type = getType(interaction);
+		int sq_provider, sq_target;
 		switch (type) {
 		case TYPE_OVERPROTECTION:
-			int sq_prodider = getBits(interaction, 6, 6);
-			int sq_target = getBits(interaction, 12, 6);
-			ret = "{"+Square.toString(sq_prodider) + " overprotects " + Square.toString(sq_target)+"}";
+			sq_provider = getBits(interaction, 6, 6);
+			sq_target = getBits(interaction, 12, 6);
+			ret = "{"+Square.toString(sq_provider) + " overprotects " + Square.toString(sq_target)+"}";
+			break;
+		case TYPE_BARELY_ADEQUATE_GUARD:
+			sq_provider = getBits(interaction, 6, 6);
+			sq_target = getBits(interaction, 12, 6);
+			ret = "{"+Square.toString(sq_provider) + " barely adequately guards " + Square.toString(sq_target)+"}";
+			break;
+		case TYPE_ADEQUATE_GUARD:
+			sq_provider = getBits(interaction, 6, 6);
+			sq_target = getBits(interaction, 12, 6);
+			ret = "{"+Square.toString(sq_provider) + " adequately guards " + Square.toString(sq_target)+"}";
 			break;
 		default:
 			ret="NOT SUPPORTED";
@@ -38,6 +51,24 @@ public class Interaction {
 		assert Square.validate(sq_provider);
 		assert Square.validate(sq_target);
 		int ret=setBits(0, TYPE_OVERPROTECTION, 0, 6);
+		ret=setBits(ret, sq_provider, 6, 6);
+		ret=setBits(ret, sq_target, 12, 6);
+		return ret;
+	}
+	
+	public static int createBarelyAdequateGuard(int sq_provider, int sq_target) {
+		assert Square.validate(sq_provider);
+		assert Square.validate(sq_target);
+		int ret=setBits(0, TYPE_BARELY_ADEQUATE_GUARD, 0, 6);
+		ret=setBits(ret, sq_provider, 6, 6);
+		ret=setBits(ret, sq_target, 12, 6);
+		return ret;
+	}
+	
+	public static int createAdequateGuard(int sq_provider, int sq_target) {
+		assert Square.validate(sq_provider);
+		assert Square.validate(sq_target);
+		int ret=setBits(0, TYPE_ADEQUATE_GUARD, 0, 6);
 		ret=setBits(ret, sq_provider, 6, 6);
 		ret=setBits(ret, sq_target, 12, 6);
 		return ret;
--- tests/analysis/InteractionTest.java
+++ tests/analysis/InteractionTest.java
@@ -16,6 +16,20 @@ class InteractionTest {
 		assertEquals("{a1 overprotects h8}", Interaction.toString(interaction));
 		interaction = Interaction.createOverprotection(Square.H8, Square.A1);
 		assertEquals("{h8 overprotects a1}", Interaction.toString(interaction));
+		
+		interaction = Interaction.createBarelyAdequateGuard(Square.C1, Square.G5);
+		assertEquals("{c1 barely adequately guards g5}", Interaction.toString(interaction));
+		interaction = Interaction.createBarelyAdequateGuard(Square.A1, Square.H8);
+		assertEquals("{a1 barely adequately guards h8}", Interaction.toString(interaction));
+		interaction = Interaction.createBarelyAdequateGuard(Square.H8, Square.A1);
+		assertEquals("{h8 barely adequately guards a1}", Interaction.toString(interaction));
+		
+		interaction = Interaction.createAdequateGuard(Square.C1, Square.G5);
+		assertEquals("{c1 adequately guards g5}", Interaction.toString(interaction));
+		interaction = Interaction.createAdequateGuard(Square.A1, Square.H8);
+		assertEquals("{a1 adequately guards h8}", Interaction.toString(interaction));
+		interaction = Interaction.createAdequateGuard(Square.H8, Square.A1);
+		assertEquals("{h8 adequately guards a1}", Interaction.toString(interaction));
 	}
 
 }
